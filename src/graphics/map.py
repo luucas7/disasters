18370f0c7ffb2d7b770dce04322b1c9805ee9b27
Luucas7 <merlin.lucas99@gmail.com>
2025-01-05 19:09:00 +0100

    Refactor Map class to separate figure creation into its own method. Simplify callback logic by utilizing the new method for generating the map figure.
    Co-authored-by: Hyprra <Hyprra@users.noreply.github.com>

--- src/graphics/map.py
+++ src/graphics/map.py
@@ -9,7 +9,7 @@ class Map:
         self.data = data
         self.geojson = geojson
 
-    def __call__(self):
+    def create_figure(self):
         counts_by_country = self.data.groupby('Country').size().reset_index(name='Disaster_Count')
         counts_by_country['Scaled_Count'] = np.log10(counts_by_country['Disaster_Count'] + 1)
         
@@ -31,33 +31,18 @@ class Map:
             autosize=True
         )
         
+        return fig
+
+    def __call__(self):
+        fig = self.create_figure()
         return html.Div(dcc.Graph(figure=fig), style={'width': '100%', 'height': '500px'})
 
 def register_map_callbacks(app, data, geojson):
+    map_instance = Map(data, geojson)
+    
     @app.callback(
         Output('map', 'figure'),
         [Input('some-input', 'value')]
     )
     def update_map(some_value):
-        counts_by_country = data.groupby('Country').size().reset_index(name='Disaster_Count')
-        counts_by_country['Scaled_Count'] = np.log10(counts_by_country['Disaster_Count'] + 1)
-        
-        fig = go.Figure(go.Choroplethmapbox(
-            geojson=geojson,
-            locations=counts_by_country['Country'],
-            z=counts_by_country['Scaled_Count'],
-            featureidkey="properties.ADMIN",
-            colorscale="Viridis",
-            marker_opacity=0.5,
-            marker_line_width=0
-        ))
-
-        fig.update_layout(
-            mapbox_style="carto-positron",
-            mapbox_zoom=1,
-            mapbox_center={"lat": 20, "lon": 0},
-            margin={"r": 0, "t": 0, "l": 0, "b": 0},
-            autosize=True
-        )
-        
-        return fig
+        return map_instance.create_figure()
